12a92e42b0483fcace0ef649a6f9a528b8d04d30
Agu803eh <agu803eh@gmail.com>
2025-03-12 08:44:14 +0100

    Banking

--- healthx.py
+++ healthx.py
@@ -1,1 +1,10 @@
 print("Hello world")
+number = 10
+print(number)
+
+#List Data Type
+languages = ['Python', 'Java', 'C++', 'C#']
+#rint(languages)
+#rint(languages[0])
+#rint(languages[1:3])
+print(languages[-1])
